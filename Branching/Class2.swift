dc3b869913ac6a51d76280b46000130672afe1ec
Jefry Eko Mulya <jefrydagucci@gmail.com>
2019-05-27 22:48:56 +0700

    [F2] add class 2

---
+++ Branching/Class2.swift
@@ -0,0 +1,13 @@
+//
+//  Class2.swift
+//  Branching
+//
+//  Created by Jefry Eko Mulya on 27/05/19.
+//  Copyright © 2019 Jefry Eko Mulya. All rights reserved.
+//
+
+import Foundation
+
+class Class2{
+    
+}
