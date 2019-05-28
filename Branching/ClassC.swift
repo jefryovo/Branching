167b7edb19a37193ed82f215b5b9118243ec520a
Jefry Eko Mulya <jefrydagucci@gmail.com>
2019-05-29 03:22:51 +0700

    [TICKET-2] add class C

---
+++ Branching/ClassC.swift
@@ -0,0 +1,13 @@
+//
+//  ClassC.swift
+//  Branching
+//
+//  Created by Jefry Eko Mulya on 29/05/19.
+//  Copyright © 2019 Jefry Eko Mulya. All rights reserved.
+//
+
+import Foundation
+
+class ClassC{
+    
+}
