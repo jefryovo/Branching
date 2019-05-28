6420a6804b2fc133af759cb3475e1e15eb58f495
Jefry Eko Mulya <jefrydagucci@gmail.com>
2019-05-29 03:27:15 +0700

    [TICKET-3] implement feature 3

---
+++ Branching/ClassD.swift
@@ -0,0 +1,19 @@
+//
+//  ClassD.swift
+//  Branching
+//
+//  Created by Jefry Eko Mulya on 29/05/19.
+//  Copyright © 2019 Jefry Eko Mulya. All rights reserved.
+//
+
+import Foundation
+
+class ClassD{
+    func methodD1(){
+        
+    }
+    
+    func methodD2(){
+        
+    }
+}
